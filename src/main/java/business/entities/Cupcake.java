c101342a89c59a166bb87f186180bb8762b03508
AndersLind99 <cph-al350@cphbusiness.dk>
2021-04-19 17:06:29 +0200

    created cupcake entity

--- src/main/java/business/entities/Cupcake.java
+++ src/main/java/business/entities/Cupcake.java
@@ -1,4 +1,76 @@
 package business.entities;
 
 public class Cupcake {
+
+    private int cupcake_id;
+    private int order_id;
+    private int toppings_id;
+    private int bases_id;
+    private int quantity;
+    private int sum;
+
+    public Cupcake(int cupcake_id, int order_id, int toppings_id, int bases_id, int quantity, int sum) {
+        this.cupcake_id = cupcake_id;
+        this.order_id = order_id;
+        this.toppings_id = toppings_id;
+        this.bases_id = bases_id;
+        this.quantity = quantity;
+        this.sum = sum;
+    }
+
+    public Cupcake(int order_id, int toppings_id, int bases_id, int quantity, int sum) {
+        this.order_id = order_id;
+        this.toppings_id = toppings_id;
+        this.bases_id = bases_id;
+        this.quantity = quantity;
+        this.sum = sum;
+    }
+
+    public int getCupcake_id() {
+        return cupcake_id;
+    }
+
+    public void setCupcake_id(int cupcake_id) {
+        this.cupcake_id = cupcake_id;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getToppings_id() {
+        return toppings_id;
+    }
+
+    public void setToppings_id(int toppings_id) {
+        this.toppings_id = toppings_id;
+    }
+
+    public int getBases_id() {
+        return bases_id;
+    }
+
+    public void setBases_id(int bases_id) {
+        this.bases_id = bases_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
 }
